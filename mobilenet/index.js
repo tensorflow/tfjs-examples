b0b41501fbe0fe84d63fd3269084deaefbda4f52
Nikhil Thorat <nsthorat@google.com>
2018-03-14 13:31:17 -0400

    snapshot

--- mobilenet/index.js
+++ mobilenet/index.js
@@ -21,7 +21,7 @@ import {IMAGENET_CLASSES} from './imagenet_classes';
 
 const MOBILENET_MODEL_PATH =
     // tslint:disable-next-line:max-line-length
-    'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
+    'https://storage.googleapis.com/tfjs-models/tfjs-layers/mobilenet_v1_0.25_224/model.json';
 
 const IMAGE_SIZE = 224;
 const TOPK_PREDICTIONS = 10;

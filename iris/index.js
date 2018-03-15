77aec02e4720a61a601f6536ae389edac247cc2b
Shanqing Cai <cais@google.com>
2018-03-14 22:21:23 -0400

    Add Iris Example (#12)
    * Add Iris Examples
    
    Supports:
    * Loading pretrained model from GCS using tf.laodModel()
    * Train a model from scratch in the browser
    * Display of loss and accuracy values during training
    * Evaluation on manually-inpupt data
    * Display of logits

---
+++ iris/index.js
@@ -0,0 +1,144 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as tf from '@tensorflow/tfjs';
+
+import {getIrisData, IRIS_CLASSES, IRIS_NUM_CLASSES} from './data';
+import {clearEvaluateTable, getManualInputData, loadTrainParametersFromUI, plotAccuracies, plotLosses, renderEvaluateTable, renderLogitsForManualInput, setManualInputWinnerMessage, status, wireUpEvaluateTableCallbacks} from './ui';
+
+let model;
+
+async function loadHostedPretrainedModel() {
+  const HOSTED_MODEL_JSON_URL =
+      'https://storage.googleapis.com/tfjs-models/tfjs-layers/iris_v1/model.json';
+  status('Loading pretrained model from ' + HOSTED_MODEL_JSON_URL);
+  try {
+    model = await tf.loadModel(HOSTED_MODEL_JSON_URL);
+    status('Done loading pretrained model.');
+  } catch (err) {
+    status('Loading pretrained model failed.');
+  }
+}
+
+async function trainModel(xTrain, yTrain, xTest, yTest) {
+  status('Training model... Please wait.');
+
+  const params = loadTrainParametersFromUI();
+
+  const model = tf.sequential();
+  model.add(tf.layers.dense(
+      {units: 10, activation: 'sigmoid', inputShape: [xTrain.shape[1]]}));
+  model.add(tf.layers.dense({units: 3, activation: 'softmax'}));
+
+  const optimizer = tf.train.adam(params.learningRate);
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  const lossValues = [];
+  const accuracyValues = [];
+  const history = await model.fit({
+    x: xTrain,
+    y: yTrain,
+    epochs: params.epochs,
+    validationData: [xTest, yTest],
+    callbacks: {
+      onEpochEnd: async (epoch, logs) => {
+        lossValues.push(
+            {'epoch': epoch, 'loss': logs['loss'].dataSync(), 'set': 'train'});
+        lossValues.push({
+          'epoch': epoch,
+          'loss': logs['val_loss'].dataSync(),
+          'set': 'validation'
+        });
+        plotLosses(lossValues);
+
+        accuracyValues.push({
+          'epoch': epoch,
+          'accuracy': logs['acc'].dataSync(),
+          'set': 'train'
+        });
+        accuracyValues.push({
+          'epoch': epoch,
+          'accuracy': logs['val_acc'].dataSync(),
+          'set': 'validation'
+        });
+        plotAccuracies(accuracyValues);
+
+        await tf.nextFrame();
+      },
+    }
+  });
+
+  status('Model training complete.');
+  return model;
+}
+
+async function predictOnManualInput(model) {
+  if (model == null) {
+    setManualInputWinnerMessage('ERROR: Please load or train model first.');
+    return;
+  }
+
+  const inputData = getManualInputData();
+  tf.tidy(() => {
+    const input = tf.tensor2d([inputData], [1, 4]);
+    const predictOut = model.predict(input);
+
+    const logits = Array.from(predictOut.dataSync());
+    const winner = IRIS_CLASSES[predictOut.argMax(-1).dataSync()[0]];
+    setManualInputWinnerMessage(winner);
+    renderLogitsForManualInput(logits);
+  });
+}
+
+async function evaluateModelOnTestData(model, xTest, yTest) {
+  clearEvaluateTable();
+  tf.tidy(() => {
+    const xData = xTest.dataSync();
+    const yTrue = yTest.argMax(-1).dataSync();
+    const predictOut = model.predict(xTest);
+    const logits = Array.from(predictOut.dataSync());
+    const yPred = predictOut.argMax(-1).dataSync();
+
+    renderEvaluateTable(xData, yTrue, yPred, logits);
+  });
+  predictOnManualInput(model);
+}
+
+async function iris() {
+  const [xTrain, yTrain, xTest, yTest] = getIrisData(0.15);
+
+  document.getElementById('train-from-scratch')
+      .addEventListener('click', async () => {
+        model = await trainModel(xTrain, yTrain, xTest, yTest);
+        evaluateModelOnTestData(model, xTest, yTest);
+      });
+
+  document.getElementById('load-pretrained')
+      .addEventListener('click', async () => {
+        clearEvaluateTable();
+        await loadHostedPretrainedModel();
+        predictOnManualInput(model);
+      });
+
+  wireUpEvaluateTableCallbacks(() => predictOnManualInput(model));
+}
+
+iris();

4d5c56671d64b6bc09b6d5382250088ea46ea0eb
David Soergel <soergel@google.com>
2018-03-20 10:22:19 -0400

    Add Basic example.

---
+++ basic/index.js
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as tf from '@tensorflow/tfjs';
+
+const model = tf.sequential();
+model.add(tf.layers.dense({inputDim: 1, units: 1, name: 'layer_1'}));
+model.add(tf.layers.dense({units: 1, name: 'layer_2'}));
+
+const layerNames = [];
+model.layers.map(function(x) {
+  layerNames.push(x.name);
+});
+document.getElementById('layers_output').innerText =
+    'Layer names: ' + layerNames.join(', ');
+
+const batchSize = 5;
+const input = tf.ones([batchSize, 1]);
+const onesMessage = document.getElementById('ones_output').innerText =
+    'Ones of size [5, 1]: ' + input.dataSync().join(', ');
+
+const result = model.apply(input);
+document.getElementById('network_output').innerText = 'Result of running ' +
+    'through a two layer network: ' + result.dataSync().join(', ');

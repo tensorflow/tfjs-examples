f5971782cb92899dc5f97a129159457061829336
nsthorat <nsthorat@google.com>
2018-03-25 16:21:41 -0400

    snapshot

--- webcam-transfer-learning/controller_dataset.js
+++ webcam-transfer-learning/controller_dataset.js
@@ -32,21 +32,27 @@ export class ControllerDataset {
    * @param {number} label The label of the example. Should be an umber.
    */
   addExample(example, label) {
-    const y = tf.tidy(() => tf.oneHot(tf.tensor1d([label]), this.numClasses));
+    tf.tidy(() => {
+      // One-hot encode the label.
+      const y = tf.oneHot(tf.tensor1d([label]), this.numClasses);
 
-    if (this.xs == null) {
-      this.xs = tf.keep(example);
-      this.ys = tf.keep(y);
-    } else {
-      const oldX = this.xs;
-      this.xs = tf.keep(oldX.concat(example, 0));
-      oldX.dispose();
+      if (this.xs == null) {
+        // For the first example that gets added, keep example and y so that the
+        // ControllerDataset owns the memory of the inputs. This makes sure that
+        // if addExample() is called in a tf.tidy(), these Tensors will not get
+        // disposed.
+        this.xs = tf.keep(example);
+        this.ys = tf.keep(y);
+      } else {
+        const oldX = this.xs;
+        this.xs = tf.keep(oldX.concat(example, 0));
 
-      const oldY = this.ys;
-      this.ys = tf.keep(oldY.concat(y, 0));
-      oldY.dispose();
+        const oldY = this.ys;
+        this.ys = tf.keep(oldY.concat(y, 0));
 
-      y.dispose();
-    }
+        oldX.dispose();
+        oldY.dispose();
+      }
+    })
   }
 }

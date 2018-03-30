83a9b924a12ecb4647f4ac813138128402195886
Stanley Bileschi <bileschi@google.com>
2018-03-30 12:02:54 -0400

    Make snippet function run in an async function, like the website snippets.

--- micro/my_snippit.js
+++ micro/my_snippit.js
@@ -14,28 +14,39 @@
 // ==============================================================================
 
 // TFJS microfiddle.
-var output = []
-console.log = function(x){
-  output.push(x);
-  document.getElementById('fiddle_out_div').innerHTML = output.join("<br>");
-}
-// Edit snippet below.
 
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-// Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-// Train the model using the data.
-for (var i = 0; i < 5; ++i) {
-  console.log(i);
-  model.fit(xs, ys);
+// Edit snippet in here.
+async function snippet() {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+  // Prepare the model for training: Specify the loss and the optimizer.
+  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+  // Generate some synthetic data for training.
+  const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+  const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+  // Train the model using the data.
+  await model.fit(xs, ys);
+  // Use the model to do inference on a data point the model hasn't see
+  model.predict(tf.tensor2d([5], [1, 1])).print();
 }
 
-// Use the model to do inference on a data point the model hasn't seen:
-model.predict(tf.tensor2d([5], [1, 1])).print();
+
+// Set up snippet environment, grab log, and release when done.
+async function executeCodeSnippet(consoleLogElement, mySnippet) {
+  consoleLogElement.innerText = '';
+  var oldLog = console.log;
+  console.log = function(logStr) {
+    consoleLogElement.innerText += logStr + '\n';
+  };
+
+  tf.ENV.engine.startScope();
+  await eval(mySnippet());
+  tf.ENV.engine.endScope();
+
+  console.log("--------------------");
+  console.log("done.");
+  console.log = oldLog;
+};
+
+logElement = document.getElementById('fiddle_out_div');
+executeCodeSnippet(logElement, snippet).then(() => {console.log("ran snip.");});

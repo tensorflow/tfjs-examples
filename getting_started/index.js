83fd0184a03a70b8602bbf5bcb95f6efe4096e7d
Nikhil Thorat <nsthorat@google.com>
2018-04-10 13:47:41 -0400

    Remove 'usBias' from model.fit in the getting_started example. (#57)

--- getting_started/index.js
+++ getting_started/index.js
@@ -25,25 +25,24 @@ async function myFirstTfjs() {
   // Create a simple model.
   const model = tf.sequential();
   model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
   // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({loss: 'meanSquaredError',
-                 optimizer: 'sgd',
-                 useBias: 'true'});
+  model.compile({
+    loss: 'meanSquaredError',
+    optimizer: 'sgd'
+  });
+
   // Generate some synthetic data for training. (y = 2x - 1)
   const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
   const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+
   // Train the model using the data.
   await model.fit(xs, ys, {epochs: 250});
+
   // Use the model to do inference on a data point the model hasn't seen.
-  // Perform within a tf.tidy block to perform cleanup on intermediate tensors.
-  tf.tidy(() => {
-    // Should print approximately 39.
-    document.getElementById('micro_out_div').innerText += model.predict(
-        tf.tensor2d([20], [1, 1]));
-  });
-  // Manually clean up the memory for these variables.
-  xs.dispose();
-  ys.dispose();
+  // Should print approximately 39.
+  document.getElementById('micro_out_div').innerText +=
+      model.predict(tf.tensor2d([20], [1, 1]));
 }
 
 myFirstTfjs();

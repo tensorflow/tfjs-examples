7276f8d590673fdaf7debbb568c06697911b00be
Nick Kreeger <nick.kreeger@gmail.com>
2018-05-04 13:55:25 -0700

    Add license headers.

--- baseball/src/client/App.vue
+++ baseball/src/client/App.vue
@@ -1,27 +1,36 @@
+<!--
+==============================================================================
+Copyright 2018 Google LLC. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================
+-->
+
 <template>
-  <div>
-
-    <div class="container content">
-      <div class="">
-
-        <div class="row d-flex">
-          <div class="col-lg-4">
-            <div class="card" v-if="predictions.length === 0">
-              <div class="card-body text-center" style="color: #80868b">
-                Waiting for live pitch data...
-              </div>
-            </div>
+  <div class="container content">
+    <div class="row d-flex">
+      <div class="col-lg-4">
+        <div class="card" v-if="predictions.length === 0">
+          <div class="card-body text-center" style="color: #80868b">
+            Waiting for live pitch data...
           </div>
         </div>
-
-        <transition-group name="list-complete">
-          <pitch class="pitch"
-                v-for="prediction in predictions"
-                :key="prediction.uuid"
-                v-bind:prediction="prediction"></pitch>
-        </transition-group>
       </div>
     </div>
+
+    <transition-group name="list-complete">
+      <pitch class="pitch"
+            v-for="prediction in predictions"
+            :key="prediction.uuid"
+            v-bind:prediction="prediction"></pitch>
+    </transition-group>
   </div>
 </template>
 
@@ -98,7 +107,4 @@ body {
   line-height: 60px;
   background-color: #f5f5f5;
 }
-/* .card-columns {
-  column-count: 3;
-} */
 </style>
--- baseball/src/client/Pitch.vue
+++ baseball/src/client/Pitch.vue
@@ -1,3 +1,17 @@
+<!--
+==============================================================================
+Copyright 2018 Google LLC. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================
+-->
 <template>
 
 <div class="row d-flex">
@@ -23,10 +37,8 @@
 <script lang="ts" src="./pitch.ts"></script>
 
 <style>
-
 .card {
   transition: 1.25s;
-  /* display: inline-block; */
   background: #ffffff;
   box-shadow: 0 1px 2px 0 rgba(60, 64, 67, 0.3),
     0 1px 3px 1px rgba(60, 64, 67, 0.15);
@@ -35,7 +47,7 @@
 }
 
 .card.correct {
-  background-color: rgba(0, 255, 0, 0.10);
+  background-color: rgba(0, 255, 0, 0.1);
 }
 
 .card.incorrect {
@@ -43,11 +55,11 @@
 }
 
 .icon.correct {
-  color: #34A853;
+  color: #34a853;
 }
 
 .icon.incorrect {
-  color: #D0021B;
+  color: #d0021b;
 }
 
 .pitch-card {

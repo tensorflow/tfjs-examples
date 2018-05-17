e77cc9f22d5e69604e8d1cc2c50b8964169f90f0
Shanqing Cai <cais@google.com>
2018-05-17 15:31:03 -0400

    Add example for saving / loading models using IndexedDB to Iris demo
    * Remove the capability to load a converted Keras model from the local
      file system, a) to avoid confusion, b) this functionality is not
      highly useful anymore.
    * Fix an issue in the widths of the vega-embed plots: they somehow
      started resizing. This PR makes then fixed, as they should be.

--- iris/serve.sh
+++ iris/serve.sh
@@ -28,14 +28,14 @@ NODE_ENV=development
 RESOURCE_PORT=1235
 
 # Ensure that http-server is available
-yarn
+# yarn
 
 echo Starting the pretrained model server...
 node_modules/http-server/bin/http-server dist --cors -p "${RESOURCE_PORT}" > /dev/null & HTTP_SERVER_PID=$!
 
 echo Starting the example html/js server...
 # This uses port 1234 by default.
-node_modules/parcel-bundler/bin/cli.js serve -d dist --open --no-hmr --public-url / index.html
+node_modules/parcel-bundler/bin/cli.js serve -d dist --open --no-hmr --public-url / index.html -p 1236
 
 # When the Parcel server exits, kill the http-server too.
 kill $HTTP_SERVER_PID

a621beb23fe4bced0f75d1d91e694fc8cff64581
Shanqing Cai <cais@google.com>
2018-06-29 23:51:07 -0400

    Add PolicyNetwork

--- cart-pole/cart_pole.js
+++ cart-pole/cart_pole.js
@@ -2,8 +2,30 @@
  * Based on: http://incompleteideas.net/book/code/pole.c
  */
 
+import * as tf from '@tensorflow/tfjs';
+
+/**
+ * Cart-pole system simulator.
+ *
+ * The system has four state variables:
+ *
+ *   - x: The 1D location of the cart.
+ *   - xDot: The velocity of the cart.
+ *   - theta: The angle of the pole (in radians). A value of 0 corresponds to
+ *     a vertical position.
+ *   - thetaDot: The angular velocity of the pole.
+ *
+ * The system is controlled through a single action.
+ */
 export class CartPole {
-  constructor() {
+
+  /**
+   *
+   * @param {bool} initializeStateRandomly Whether to initialize the state
+   *   randomlly. If `false` (default), all states will be initialized to
+   *   zero.
+   */
+  constructor(initializeStateRandomly) {
     this.gravity = 9.8;
     this.massCart = 1.0;
     this.massPole = 0.1;
@@ -19,13 +41,39 @@ export class CartPole {
     this.xDot = 0;  // Cart velocity.
     this.theta = 0;  // Pole angle, radians.
     this.thetaDot = 0;  // Pole angle velocity.
+    if (initializeStateRandomly) {
+      this.setRandomState_();
+    }
   }
 
-  setRandomState() {
+  /**
+   * Set the state of the cart-pole system randomly.
+   */
+  setRandomState_() {
     this.x = Math.random() - 0.5;
+    this.xDot = (Math.random() - 0.5) * 1;
     this.theta = (Math.random() - 0.5) * (Math.PI / 2 * 0.3);
+    this.thetaDot =  (Math.random() - 0.5) * 0.5;
   }
 
+  /**
+   * Get current state as a tf.Tensor of shape [1, 4].
+   */
+  getStateTensor() {
+    const buffer = new tf.TensorBuffer([1, 4]);
+    buffer.set(this.x, 0, 0);
+    buffer.set(this.xDot, 0, 1);
+    buffer.set(this.theta, 0, 2);
+    buffer.set(this.thetaDot, 0, 3);
+    return buffer.toTensor();
+  }
+
+  /**
+   * Update the cart-pole system using an action.
+   * @param {number} action Only the sign of `action` matters.
+   *   A value > 0 leads to a rightward force of a fixed magnitude.
+   *   A value <= 0 leads to a leftward force of the same fixed magnitude.
+   */
   update(action) {
     const force = action > 0 ? this.forceMag : -this.forceMag;
 
@@ -52,6 +100,11 @@ export class CartPole {
         `theta = ${this.theta}; thetaDot = ${this.theta}`);  // DEBUG
   }
 
+  /**
+   * Render the current state of the system on an HTML canvas.
+   *
+   * @param {HTMLCanvasElement} canvas
+   */
   render(canvas) {
     const X_MIN = -2;
     const X_MAX = 2;
--- cart-pole/index.js
+++ cart-pole/index.js
@@ -1,13 +1,55 @@
-
+import * as tf from '@tensorflow/tfjs';
 import {CartPole} from './cart_pole';
+
+/**
+ * Policy network for controlling the cart-pole system.
+ */
+class PolicyNetwork {
+  /**
+   * Constructor of PolicyNetwork.
+   *
+   * @param {number} hiddenLayerSize Size of the hidden layer.
+   */
+  constructor(hiddenLayerSize) {
+    this.model_ = tf.sequential();
+    this.model_.add(tf.layers.dense({
+      units: hiddenLayerSize,
+      activation: 'elu',
+      inputShape: [4]
+    }));
+    this.model_.add(tf.layers.dense({units: 1}));
+  }
+
+  /**
+   * Get action based  on a state tensor.
+   *
+   * @param {tf.Tensor} inputs A tf.Tensor instance of shape `[batchSize, 4]`.
+   * @returns {Float32Array} 0-1 action values for all the examples in the batch,
+   *   length = batchSize.
+   */
+  getActions(inputs) {
+    return tf.tidy(() => {
+      const output = this.model_.predict(inputs);
+
+      // Get the probability of the left word action.
+      const leftProb = tf.sigmoid(output);
+      // Probabilites of the left and right actions.
+      const leftRightProbs =
+          tf.concat([leftProb, tf.sub(tf.onesLike(leftProb), leftProb)], 1);
+      leftRightProbs.print();  // DEBUG
+      const actions = tf.multinomial(leftRightProbs, 1, null, true).dataSync();
+      return actions;
+    });
+  }
+}
 
 const cartPoleCanvas = document.getElementById('cart-pole-canvas');
 const leftButton = document.getElementById('left');
 const rightButton = document.getElementById('right');
-const cartPole = new CartPole();
+const stepButton = document.getElementById('step');
+const cartPole = new CartPole(true);
 console.log(cartPole);  // DEBUG
 
-cartPole.setRandomState();
 cartPole.render(cartPoleCanvas);
 
 leftButton.addEventListener('click', () => {
@@ -17,5 +59,14 @@ leftButton.addEventListener('click', () => {
 
 rightButton.addEventListener('click', () => {
   cartPole.update(1);
+  cartPole.render(cartPoleCanvas);
+});
+
+
+const policyNet =  new PolicyNetwork(5);
+
+stepButton.addEventListener('click', () => {
+  const action = policyNet.getActions(cartPole.getStateTensor())[0];
+  cartPole.update(action);
   cartPole.render(cartPoleCanvas);
 });

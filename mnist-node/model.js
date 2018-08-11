d048f0c52fa05871b4a34883f12d8b4397834d43
Shanqing Cai <cais@google.com>
2018-08-11 15:01:34 -0400

    Beef up model

--- mnist-node/model.js
+++ mnist-node/model.js
@@ -20,24 +20,27 @@ const tf = require('@tensorflow/tfjs');
 const model = tf.sequential();
 model.add(tf.layers.conv2d({
   inputShape: [28, 28, 1],
-  kernelSize: 5,
-  filters: 8,
-  strides: 1,
+  filters: 32,
+  kernelSize: 3,
   activation: 'relu',
-  kernelInitializer: 'varianceScaling'
 }));
 model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
 model.add(tf.layers.conv2d({
-  kernelSize: 5,
-  filters: 16,
-  strides: 1,
+  filters: 64,
+  kernelSize: 3,
   activation: 'relu',
-  kernelInitializer: 'varianceScaling'
 }));
 model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+model.add(tf.layers.conv2d({
+  filters: 64,
+  kernelSize: 3,
+  activation: 'relu',
+}));
 model.add(tf.layers.flatten());
-model.add(tf.layers.dense(
-    {units: 10, kernelInitializer: 'varianceScaling', activation: 'softmax'}));
+model.add(tf.layers.dropout({rate: 0.25}));
+model.add(tf.layers.dense({units: 512, activation: 'relu'}));
+model.add(tf.layers.dropout({rate: 0.5}));
+model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
 
 const optimizer = 'rmsprop';
 model.compile({

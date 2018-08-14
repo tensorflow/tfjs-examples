0bac790965e3cf3450a93496b011ccc0265833d2
Shanqing Cai <cais@google.com>
2018-08-14 12:34:25 -0400

    fix typo;

--- mnist-node/main.js
+++ mnist-node/main.js
@@ -74,7 +74,7 @@ async function main(epochs, batchSize, modelSavePath) {
   console.log(
       `\nEvaluation result:\n` +
       `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; `+
-      `Acurracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`);
+      `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`);
 
   if (modelSavePath != null) {
     await model.save(`file://${modelSavePath}`);

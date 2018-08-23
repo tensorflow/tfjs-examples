0f90e30ab935dd0b2edb209e28ebdf26a21dfdc9
Shanqing Cai <cais@google.com>
2018-08-23 15:01:58 -0400

    Use argparser and parameterize some options: (#127)
    * Use argparser and parameterize some options:
    
    including:
    * Number of training epochs
    * Batch size for training
    * Whether and where the model will be saved.

--- mnist-node/main.js
+++ mnist-node/main.js
@@ -17,12 +17,13 @@
 
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
+const argparse = require('argparse');
 const ProgressBar = require('progress');
 
 const data = require('./data');
 const model = require('./model');
 
-async function run() {
+async function run(epochs, batchSize, modelSavePath) {
   await data.loadData();
 
   const {images: trainImages, labels: trainLabels} = data.getTrainData();
@@ -31,8 +32,6 @@ async function run() {
   let progressBar;
   let epochBeginTime;
   let millisPerStep;
-  const epochs = 20;
-  const batchSize = 128;
   const validationSplit = 0.15;
   const numTrainExamplesPerEpoch =
       trainImages.shape[0] * (1 - validationSplit);
@@ -72,10 +71,35 @@ async function run() {
   const {images: testImages, labels: testLabels} = data.getTestData();
   const evalOutput = model.evaluate(testImages, testLabels);
 
-  console.log('\nEvaluation result:');
   console.log(
+      `\nEvaluation result:\n` +
       `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; `+
-      `Acurracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`);
+      `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`);
+
+  if (modelSavePath != null) {
+    await model.save(`file://${modelSavePath}`);
+    console.log(`Saved model to path: ${modelSavePath}`);
+  }  
 }
 
-run();
+const parser = new argparse.ArgumentParser({
+  description: 'TensorFlow.js-Node MNIST Example.',
+  addHelp: true
+});
+parser.addArgument('--epochs', {
+  type: 'int',
+  defaultValue: 20,
+  help: 'Number of epochs to train the model for.'
+});
+parser.addArgument('--batch_size', {
+  type: 'int',
+  defaultValue: 128,
+  help: 'Batch size to be used during model training.'
+})
+parser.addArgument('--model_save_path', {
+  type: 'string',
+  help: 'Path to which the model will be saved after training.'
+});
+const args = parser.parseArgs();
+
+run(args.epochs, args.batch_size, args.model_save_path);

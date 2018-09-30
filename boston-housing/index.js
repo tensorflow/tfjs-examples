f3964999fad0ca83c06e91798921ac8546dd5a16
Shanqing Cai <cais@google.com>
2018-09-30 16:13:51 -0400

    Add a one-hidden-layer NN option; use leCunNormal initializer
    * Adding the one-hidden-layer NN fits the teaching material
    * leCunNormal initailizer leads to more stable and on-average better
      training results from the one-hidden-layer and two-hidden-layer NNs.
    
      For example, see final test-set MSEs from:
    
      - 2 hidden layers, default kernel initializer:
        17.2148, 12.8895, 13.1877, 13.5261
      - 2 hidden layers, leCunNormal kernel initializer:
        12.1388, 11.4624, 13.4111, 12.1905

--- boston-housing/index.js
+++ boston-housing/index.js
@@ -51,9 +51,31 @@ export const arraysToTensors = () => {
  *
  * @returns {tf.Sequential} The linear regression model.
  */
-export const linearRegressionModel = () => {
+export function linearRegressionModel() {
   const model = tf.sequential();
   model.add(tf.layers.dense({inputShape: [bostonData.numFeatures], units: 1}));
+
+  model.summary();
+  return model;
+};
+
+/**
+ * Builds and returns Multi Layer Perceptron Regression Model
+ * with 1 hidden layers, each with 10 units activated by sigmoid.
+ *
+ * @returns {tf.Sequential} The multi layer perceptron regression model.
+ */
+export function multiLayerPerceptronRegressionModel1Hidden() {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({
+    inputShape: [bostonData.numFeatures],
+    units: 50,
+    // activation: 'sigmoid',
+    kernelInitializer: 'leCunNormal'
+  }));
+  model.add(tf.layers.dense({units: 1}));
+
+  model.summary();
   return model;
 };
 
@@ -61,18 +83,24 @@ export const linearRegressionModel = () => {
  * Builds and returns Multi Layer Perceptron Regression Model
  * with 2 hidden layers, each with 10 units activated by sigmoid.
  *
- * @returns {tf.Sequential} The multi layer perceptron regression model.
+ * @returns {tf.Sequential} The multi layer perceptron regression mode  l.
  */
-export const multiLayerPerceptronRegressionModel = () => {
+export function multiLayerPerceptronRegressionModel2Hidden() {
   const model = tf.sequential();
   model.add(tf.layers.dense({
     inputShape: [bostonData.numFeatures],
     units: 50,
-    activation: 'sigmoid'
+    activation: 'sigmoid',
+    kernelInitializer: 'leCunNormal'
   }));
-  model.add(tf.layers.dense({units: 50, activation: 'sigmoid'}));
+  model.add(tf.layers.dense({
+    units: 50,
+    activation: 'sigmoid',
+    kernelInitializer: 'leCunNormal'
+  }));
   model.add(tf.layers.dense({units: 1}));
 
+  model.summary();
   return model;
 };
 

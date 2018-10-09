4dcaaa82bad56cc71d0a3821dbcb860f208f2709
Shanqing Cai <cais@google.com>
2018-10-09 17:23:15 -0400

    Show best practice of shuffling data (#147)

--- iris/data.js
+++ iris/data.js
@@ -99,17 +99,32 @@ function convertToTensors(data, targets, testSplit) {
     throw new Error('data and split have different numbers of examples');
   }
 
+  // Randomly shuffle `data` and `targets`.
+  const indices = [];
+  for (let i = 0; i < numExamples; ++i) {
+    indices.push(i);
+  }
+  tf.util.shuffle(indices);
+
+  const shuffledData = [];
+  const shuffledTargets = [];
+  for (let i = 0; i < numExamples; ++i) {
+    shuffledData.push(data[indices[i]]);
+    shuffledTargets.push(targets[indices[i]]);
+  }
+
+  // Split the data into a training set and a tet set, based on `testSplit`.
   const numTestExamples = Math.round(numExamples * testSplit);
   const numTrainExamples = numExamples - numTestExamples;
 
-  const xDims = data[0].length;
+  const xDims = shuffledData[0].length;
 
   // Create a 2D `tf.Tensor` to hold the feature data.
-  const xs = tf.tensor2d(data, [numExamples, xDims]);
+  const xs = tf.tensor2d(shuffledData, [numExamples, xDims]);
 
   // Create a 1D `tf.Tensor` to hold the labels, and convert the number label
   // from the set {0, 1, 2} into one-hot encoding (.e.g., 0 --> [1, 0, 0]).
-  const ys = tf.oneHot(tf.tensor1d(targets).toInt(), IRIS_NUM_CLASSES);
+  const ys = tf.oneHot(tf.tensor1d(shuffledTargets).toInt(), IRIS_NUM_CLASSES);
 
   // Split the data into training and test sets, using `slice`.
   const xTrain = xs.slice([0, 0], [numTrainExamples, xDims]);

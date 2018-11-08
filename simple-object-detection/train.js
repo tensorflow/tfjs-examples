f4f49b1b1fb60d2d9404860890a08fdd7e0d02ad
Shanqing Cai <cais@google.com>
2018-11-08 14:01:57 -0500

    Respond to review comments

--- simple-object-detection/train.js
+++ simple-object-detection/train.js
@@ -15,23 +15,26 @@
  * =============================================================================
  */
 
+const fs = require('fs');
+const path = require('path');
+
+const argparse = require('argparse');
 const canvas = require('canvas');
 const tf = require('@tensorflow/tfjs');
 const synthesizer = require('./synthetic_images');
 const fetch = require('node-fetch');
 require('@tensorflow/tfjs-node');
 
-// For http/https model loading with tf.loadModel.
 global.fetch = fetch;
 
 // Name prefixes of layers that will be unfrozen during fine-tuning.
-const topLayerGroupNames = ['conv_pw_10', 'conv_pw_11'];
+const topLayerGroupNames = ['conv_pw_9', 'conv_pw_10', 'conv_pw_11'];
 
-// Name of the layer that will become the top layer of the truncated base.
+// Name of the layer that will become the top layer of the decapitated base.
 const topLayerName =
     `${topLayerGroupNames[topLayerGroupNames.length - 1]}_relu`;
 
-const classLossMultiplier = tf.scalar(500);
+const classLossMultiplier = tf.scalar(100);
 
 /**
  * Custom loss function for object detection.
@@ -42,15 +45,8 @@ const classLossMultiplier = tf.scalar(500);
  *   approximatey.
  * - bounding-box loss, computed as the meanSquaredError between the
  *   true and predicted bounding boxes.
- * @param {*} yTrue The true labels as a Tensor of shape [numExamples, 5]
- *   and dtype float32.
- *   The first column is assumed to be the 0-1 indicator for whether the
- *   object is a triangle (0) or a rectangle (1).
- *   The remaining columsn are assumed to be the bounding boxes (left, right,
- *   top, and bottom, in that order).
- * @param {*} yPred The predicted labels as a Tensor of the shape shape and
- *   dtype as `yTrue`. The assumed format of the tensor is the same as that of
- *   `yTrue` as well.
+ * @param {*} yTrue
+ * @param {*} yPred
  */
 function customLossFunction(yTrue, yPred) {
   return tf.tidy(() => {
@@ -83,17 +79,17 @@ function customLossFunction(yTrue, yPred) {
  * Also gets handles to the layers that will be unfrozen during the fine-tuning
  * phase of the training.
  */
-async function loadTruncatedMobilenet() {
+async function loadDecapitatedMobilenet() {
   const mobilenet = await tf.loadModel(
       'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
 
   // Return a model that outputs an internal activation.
   const fineTuningLayers = [];
   const layer = mobilenet.getLayer(topLayerName);
-  const truncatedBase =
+  const decapitatedBase =
       tf.model({inputs: mobilenet.inputs, outputs: layer.output});
   // Freeze the model's layers.
-  for (const layer of truncatedBase.layers) {
+  for (const layer of decapitatedBase.layers) {
     layer.trainable = false;
     for (const groupName of topLayerGroupNames) {
       if (layer.name.indexOf(groupName) === 0) {
@@ -106,69 +102,82 @@ async function loadTruncatedMobilenet() {
   tf.util.assert(
       fineTuningLayers.length > 1,
       `Did not find any layers that match the prefixes ${topLayerGroupNames}`);
-  return {truncatedBase, fineTuningLayers};
-}
-
-/**
- * Build a new head (i.e., output sub-model) that will be connected to
- * the top of the truncated base for object detection.
- * 
- * @param {tf.Shape} inputShape Input shape of the new model.
- */
-function buildNewHead(inputShape) {
-  const newHead = tf.sequential();
-  newHead.add(tf.layers.flatten({inputShape}));
-  newHead.add(tf.layers.dense({units: 200, activation: 'relu'}));
-  newHead.add(tf.layers.dense({units: 5, kernelInitializer}));
-  return newHead;
+  return {decapitatedBase, fineTuningLayers};
 }
 
 /**
  * Builds object-detection model from MobileNet.
  */
 async function buildObjectDetectionModel() {
-  const {truncatedBase, fineTuningLayers} = await loadTruncatedMobilenet();
+  const {decapitatedBase, fineTuningLayers} = await loadDecapitatedMobilenet();
 
   // Build the new head model.
-  const newHead = buildNewHead(truncatedBase.outputs[0].shape.slice(1));
-
-  // Connect the new head model with the truncated base model.
-  const newOutput = newHead.apply(truncatedBase.outputs[0]);
-
-  // Construct the end-to-end model for object detection.
-  const model = tf.model({inputs: truncatedBase.inputs, outputs: newOutput});
+  const newHead = tf.sequential();
+  newHead.add(tf.layers.flatten(
+      {inputShape: decapitatedBase.outputs[0].shape.slice(1)}));
+  newHead.add(tf.layers.dense(
+      {units: 200, activation: 'relu', kernelInitializer: 'leCunNormal'}));
+  newHead.add(tf.layers.dense({units: 5, kernelInitializer: 'leCunNormal'}));
+  const newOutput = newHead.apply(decapitatedBase.outputs[0]);
+  const model = tf.model({inputs: decapitatedBase.inputs, outputs: newOutput});
 
   return {model, fineTuningLayers};
 }
 
 (async function main() {
+  // Data-related settings.
   const canvasSize = 224;  // Matches the input size of MobileNet.
-  const numExamples = 10000;
-  const validationSplit = 0.15;
   const numCircles = 10;
   const numLines = 10;
 
-  const batchSize = 128;
-  const initialTransferEpochs = 50;
-  const fineTuningEpochs = 100;
+  const parser = new argparse.ArgumentParser();
+  parser.addArgument(
+      '--numExamples',
+      {type: 'int', defaultValue: 2000, help: 'Number of training exapmles'});
+  parser.addArgument('--validationSplit', {
+    type: 'int',
+    defaultValue: 0.15,
+    help: 'Validation split to be used during training'
+  });
+  parser.addArgument('--batchSize', {
+    type: 'int',
+    defaultValue: 128,
+    help: 'Batch size to be used during training'
+  });
+  parser.addArgument('--initialTransferEpochs', {
+    type: 'int',
+    defaultValue: 100,
+    help: 'Number of training epochs in the initial transfer ' +
+        'learning (i.e., 1st) phase'
+  });
+  parser.addArgument('--fineTuningEpochs', {
+    type: 'int',
+    defaultValue: 100,
+    help: 'Number of training epochs in the fine-tuning (i.e., 2nd) phase'
+  });
+  const args = parser.parseArgs();
+
+  // Traininng related settings.
   const modelSaveURL = 'file://./dist/object_detection_model';
 
-  console.log(`Generating ${numExamples} training examples...`);
+  const tBegin = tf.util.now();
+  console.log(`Generating ${args.numExamples} training examples...`);
   const synthDataCanvas = canvas.createCanvas(canvasSize, canvasSize);
   const synth =
       new synthesizer.ObjectDetectionImageSynthesizer(synthDataCanvas, tf);
   const {images, targets} =
-      await synth.generateExampleBatch(numExamples, numCircles, numLines);
+      await synth.generateExampleBatch(args.numExamples, numCircles, numLines);
 
   const {model, fineTuningLayers} = await buildObjectDetectionModel();
-  model.compile({loss: customLossFunction, optimizer: 'rmsprop'});
+  model.compile({loss: customLossFunction, optimizer: tf.train.rmsprop(5e-3)});
   model.summary();
 
   // Initial phase of transfer learning.
+  console.log('Phase 1 of 2: initial transfer learning');
   await model.fit(images, targets, {
-    epochs: initialTransferEpochs,
-    batchSize,
-    validationSplit,
+    epochs: args.initialTransferEpochs,
+    batchSize: args.batchSize,
+    validationSplit: args.validationSplit
   });
 
   // Fine-tuning phase of transfer learning.
@@ -176,20 +185,28 @@ async function buildObjectDetectionModel() {
   for (const layer of fineTuningLayers) {
     layer.trainable = true;
   }
-  model.compile({loss: customLossFunction, optimizer: 'rmsprop'});
+  model.compile({loss: customLossFunction, optimizer: tf.train.rmsprop(2e-3)});
   model.summary();
 
   // Do fine-tuning.
+  console.log('Phase 2 of 2: fine-tuning phase');
   await model.fit(images, targets, {
-    epochs: fineTuningEpochs,
-    batchSize: batchSize / 2,
-    validationSplit,
+    epochs: args.fineTuningEpochs,
+    batchSize: arg.batchSize / 2,
+    validationSplit: args.validationSplit
   });
 
   // Save model.
+  // First make sure that the base directory dists.
+  const modelSavePath = modelSaveURL.replace('file://', '');
+  const dirName = path.dirname(modelSavePath);
+  if (!fs.existsSync(dirName)) {
+    fs.mkdirSync(dirName);
+  }
   await model.save(modelSaveURL);
+  console.log(`Model training took ${(tf.util.now() - tBegin) / 1e3} s`);
   console.log(`Trained model is saved to ${modelSaveURL}`);
   console.log(
       `\nNext, run the following command to test the model in the browser:`);
-  console.log(`yarn watch`);
+  console.log(`\n  yarn watch`);
 })();

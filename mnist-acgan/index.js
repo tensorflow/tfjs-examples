df60fc07549116fcb033523a9a496ed5e9fc10cb
Shanqing Cai <cais@google.com>
2018-11-30 11:22:30 -0500

    Add sliders for z-space

--- mnist-acgan/index.js
+++ mnist-acgan/index.js
@@ -22,10 +22,54 @@
 
 import * as tf from '@tensorflow/tfjs';
 
+import {loadMnistData, sampleFromMnistData} from './web-data';
+
 const status = document.getElementById('status');
 // const loadHostedModel = document.getElementById('load-hosted-model');
 const testModel = document.getElementById('test');
-const canvas = document.getElementById('data-canvas');
+const zSpaceSpan = document.getElementById('z-space-span');
+const fakeImagesSpan = document.getElementById('fake-images-span');
+const fakeCanvas = document.getElementById('fake-canvas');
+const realCanvas = document.getElementById('real-canvas');
+
+/**
+ * Generate values for the latent vector and show them with the sliders.
+ *
+ * @param {bool} fixedLatent Whether to use fixed value for the latent
+ *   vector (0.5 for every dimension).
+ */
+function generateLatentVector(fixedLatent) {
+  return tf.tidy(() => {
+    const latentDims = latentSliders.length;
+
+    // Generate random latent vector (a.k.a, z-space vector).
+    const latentValues = [];
+    for (let i = 0; i < latentDims; ++i) {
+      const latentValue = fixedLatent === true ? 0.5 : Math.random();
+      latentValues.push(latentValue);
+      latentSliders[i].value = latentValue;
+    }
+  });
+}
+
+/**
+ * Read the value of the latent-space vector fromthe sliders.
+ *
+ * @param {number} numRepeats Number of times to tile the single latent vector
+ *   for. Used for generating a batch of fake MNIST images.
+ * @returns The tiled latent-space vector, of shape [numRepeats, latentDim].
+ */
+function getLatentVectors(numRepeats) {
+  return tf.tidy(() => {
+    const latentDims = latentSliders.length;
+    const zs = [];
+    for (let i = 0; i < latentDims; ++i) {
+      zs.push(latentSliders[i].value);
+    }
+    const singleLatentVector = tf.tensor2d(zs, [1, latentDims]);
+    return singleLatentVector.tile([numRepeats, 1]);
+  });
+}
 
 /**
  * Generate a set of examples using the generator model of the ACGAN.
@@ -38,25 +82,68 @@ async function generateAndVisualizeImages(generator) {
       `Expected model to have exactly 2 symbolic inputs, ` +
           `but there are ${generator.inputs.length}`);
 
-  const combinedImage = tf.tidy(() => {
-    const latentDims = generator.inputs[0].shape[1];
-    // Random latent vector (a.k.a, z-space vector).
-    const noise = tf.randomUniform([10, latentDims]);
+  const combinedFakes = tf.tidy(() => {
+    const latentVectors = getLatentVectors(10);
+
     // Generate one fake image for each digit.
     const sampledLabels = tf.tensor2d([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [10, 1]);
     // The output has pixel values in the [-1, 1] interval. Normalize it
     // to the unit inerval ([0, 1]).
+    const t0 = tf.util.now();
     const generateImages =
-        generator.predict([noise, sampledLabels]).add(1).div(2);
+        generator.predict([latentVectors, sampledLabels]).add(1).div(2);
+    const elapsed = tf.util.now() - t0;
+    fakeImagesSpan.textContent =
+        `Fake images (generation took ${elapsed.toFixed(2)} ms)`;
     // Concatenate the images horizontally into a single image.
     return tf.concat(tf.unstack(generateImages), 1);
   });
 
-  await tf.toPixels(combinedImage, canvas);
-  tf.dispose(combinedImage);
+  await tf.toPixels(combinedFakes, fakeCanvas);
+  tf.dispose(combinedFakes);
+}
+
+/** Refresh examples of real MNIST images. */
+async function drawReals() {
+  const combinedReals = sampleFromMnistData(10);
+  await tf.toPixels(combinedReals, realCanvas);
+  tf.dispose(combinedReals);
+}
+
+/** An array that holds all sliders for the latent-space values. */
+let latentSliders;
+
+/**
+ * Create sliders for the latent space.
+ *
+ * @param {tf.Model} generator The generator part of the trained ACGAN.
+ */
+function createSliders(generator) {
+  const latentDims = generator.inputs[0].shape[1];
+  latentSliders = [];
+  const slidersContainer = document.getElementById('sliders-container');
+  for (let i = 0; i < latentDims; ++i) {
+    const slider = document.createElement('input');
+    slider.setAttribute('type', 'range');
+    slider.min = 0;
+    slider.max = 1;
+    slider.step = 0.01;
+    slider.value = 0.5;
+    slider.addEventListener('change', () => {
+      generateAndVisualizeImages(generator);
+    });
+
+    slidersContainer.appendChild(slider);
+    latentSliders.push(slider);
+  }
+  zSpaceSpan.textContent = `z-space vector (${latentDims} dimensions)`;
 }
 
 async function init() {
+  // Load MNIST data for display in webpage.
+  status.textContent = 'Loading MNIST data...';
+  await loadMnistData();
+
   const LOCAL_MEATADATA_PATH = 'generator/acgan-metadata.json';
   const LOCAL_MODEL_PATH = 'generator/model.json';
   // TODO(cais): Add URL to hosted model and logic for loading it.
@@ -66,7 +153,7 @@ async function init() {
   // "Load hosted model" button.
   let model;
   try {
-    console.log('Loading metadata');
+    status.textContent = 'Loading metadata';
     const metadata =
         await (await fetch(LOCAL_MEATADATA_PATH, {cache: 'no-cache'})).json();
 
@@ -75,6 +162,9 @@ async function init() {
         tf.io.browserHTTPRequest(LOCAL_MODEL_PATH, {cache: 'no-cache'}));
     model.summary();
 
+    // Create slider for the z-space (latent vectors).
+    createSliders(model);
+
     testModel.disabled = false;
     if (metadata.completed) {
       status.textContent =
@@ -82,11 +172,15 @@ async function init() {
           `is completed. `;
     } else {
       status.textContent = `Training of ACGAN in Node.js is ongoing (epoch ` +
-          `${metadata.currentEpoch + 1}/${metadata.totalEpochs}). `;
+          `${metadata.currentEpoch + 1}/${metadata.totalEpochs})... `;
     }
-    status.textContent += 'Loaded locally-saved model! Now click "Generate".'
+    status.textContent +=
+        'Loaded locally-saved model! Now click "Generate" or ' +
+        'adjust the z-space sliders.';
 
+    generateLatentVector(true);
     generateAndVisualizeImages(model);
+    drawReals();
   } catch (err) {
     status.textContent =
         'Failed to load locally-saved model and/or metadata. ' +
@@ -113,7 +207,11 @@ async function init() {
   //   }
   // });
 
-  testModel.addEventListener('click', () => generateAndVisualizeImages(model));
+  testModel.addEventListener('click', () => {
+    generateLatentVector(false);
+    generateAndVisualizeImages(model);
+    drawReals();
+  });
 }
 
 init();

455b5cf63ca730da604ce65bae8eb8b8a2ddba4d
Shanqing Cai <cais@google.com>
2019-01-01 12:24:22 -0500

    Save

--- visualize-convnet/main.js
+++ visualize-convnet/main.js
@@ -20,116 +20,174 @@
  * https://github.com/fchollet/deep-learning-with-python-notebooks/blob/master/5.4-visualizing-what-convnets-learn.ipynb
  */
 
+const argparse = require('argparse');
 const fs = require('fs');
-const Jimp = require('jimp');
-
+const path = require('path');
+const shelljs = require('shelljs');
 const tf = require('@tensorflow/tfjs');
+const utils = require('./utils');
 
-require('@tensorflow/tfjs-node-gpu');
-// TODO(cais): Try the tfjs-node version of this.
-
+// TODO(cais): Clean up.
 // const MODEL_PATH =
 //     // tslint:disable-next-line:max-line-length
 //     'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
 // const MODEL_PATH = 'https://storage.googleapis.com/tfjs-speech-model-test/mobilenetv2_tfjs/model.json';
 // const MODEL_PATH = 'https://storage.googleapis.com/tfjs-speech-model-test/mobilenetv2_0.35_tfjs/model.json';
 // const MODEL_PATH = 'https://storage.googleapis.com/tfjs-models/tfjs/mnist_transfer_cnn_v1/model.json';
-const MODEL_PATH = 'file://./vgg16_tfjs/model.json';
+// const MODEL_PATH = 'file://./vgg16_tfjs/model.json';
 
-const epsilon = tf.scalar(1e-5);
+const EPSILON = 1e-5;  // "Fudge" factor to prevent division by zero.
 
 // const filterCanvas = document.getElementById('filter');
 
 // TODO(cais): Deduplicate with index.js.
-async function generatePatterns(model, layerName, filterIndex) {
-  const imageH = model.inputs[0].shape[1];
-  const imageW = model.inputs[0].shape[2];
-  const imageDepth = model.inputs[0].shape[3];
+// TODO(cais): Get rid of filterIndex.
+// TODO(cais): Doc string.
+/**
+ * Generate the maximally-activating input image for a conv2d layer filter.
+ * 
+ * Uses gradient ascent in input space.
+ * 
+ * @param {tf.Model} model The model that the conv2d layer of interest belongs
+ *   to.
+ * @param {string} layerName Name of the layer.
+ * @param {number} filterIndex Index to the filter of interest. Must be
+ *   < number of filters of the conv2d layer.
+ * @param {number} iterations Number of gradient-ascent iterations.
+ * @return {tf.Tensor} The maximally-activating input image as a tensor.
+ */
+function inputGradientAscent(model, layerName, filterIndex, iterations = 40) {
+  return tf.tidy(() => {
+    const imageH = model.inputs[0].shape[1];
+    const imageW = model.inputs[0].shape[2];
+    const imageDepth = model.inputs[0].shape[3];
 
-  const layerOutput = model.getLayer(layerName).output;
-  const auxModel = tf.model({inputs: model.inputs, outputs: layerOutput});
-  const lossFunction = (input) => {
-    // ----------------------------------
-    // console.log(`Calling apply(): input.shape: ${input.shape}`);  // DEBUG
-    const out = auxModel.apply(input, {training: true}).gather([filterIndex], 3);
-    return out;
-  }  // TODO(cais): Simplify.
+    const layerOutput = model.getLayer(layerName).output;
+    const auxModel = tf.model({inputs: model.inputs, outputs: layerOutput});
+    const lossFunction =
+        (input) => auxModel.apply(input, {training: true}).gather([filterIndex], 3);
+  
+    let image = tf.randomUniform([1, imageH, imageW, imageDepth], 0, 1).mul(20).add(128);
+    const gradients = tf.grad(lossFunction);
 
-  let image = tf.tidy(() => tf.randomUniform([1, imageH, imageW, imageDepth], 0, 1).mul(20).add(128));
-  const gradients = tf.grad(lossFunction);
-
-  const iterations = 100;  // TODO(cais): 40.
-  const stepSize = 1;
-  for (let i = 0; i < iterations; ++i) {
-    console.log(`Iteration ${i + 1}/${iterations}`);  // DEBUG
-    const scaledGrads = tf.tidy(() => {
-      const grads = gradients(image);
-      // console.log(`grads.shape: ${JSON.stringify(grads.shape)}`);  // DEBUG
-      const norm = tf.sqrt(tf.mean(tf.square(grads))).add(epsilon);
-      // console.log(`norm.shape: ${JSON.stringify(norm.shape)}`);  // DEBUG
-      // norm.print();
-      return grads.div(norm);
-    });
-    const newInputImage = tf.tidy(() => image.add(scaledGrads.mul(stepSize)).clipByValue(0, 255));
-    scaledGrads.dispose();
-    image.dispose();
-    image = newInputImage;
-    // clipByValue(0, 255)
-    // const filterImage = tf.tidy(() => inputImage.squeeze([0]).asType('int32'));
-    // await tf.toPixels(filterImage, filterCanvas);
-    // filterImage.dispose();
-    // console.log(tf.memory().numTensors);  // DEBUG
-  }
-
-  const imageData = image.clipByValue(0, 255).asType('int32').dataSync();
-  console.log(imageData.length);
-  const bufferLen = imageH * imageW * 4;
-  const buffer = new Uint8Array(bufferLen);
-  let index = 0;
-  for (let i = 0; i < imageH; ++i) {
-    for (let j = 0; j < imageW; ++j) {
-      const inIndex = 3 * (i * imageW + j);
-      buffer.set([imageData[inIndex]], index++);
-      buffer.set([imageData[inIndex + 1]], index++);
-      buffer.set([imageData[inIndex + 2]], index++);
-      buffer.set([255], index++);
+    const stepSize = 1;
+    for (let i = 0; i < iterations; ++i) {
+      // console.log(`Iteration ${i + 1}/${iterations}`);  // DEBUG
+      const scaledGrads = tf.tidy(() => {
+        const grads = gradients(image);
+        const norm = tf.sqrt(tf.mean(tf.square(grads))).add(EPSILON);
+        return grads.div(norm);
+      });
+      const newInputImage = image.add(scaledGrads.mul(stepSize));
+      scaledGrads.dispose();
+      image.dispose();
+      image = newInputImage;
     }
-  }
-  console.log(`index = ${index}`);
-
-  new Jimp({data: new Buffer(buffer), width: imageW, height: imageH}, (err, img) => {
-    console.log(img);  // DEBUG
-    img.write('filter.png');
+    return deprocessImage(image);
   });
+}
 
-//   console.log(inputImage.dataSync());
-//   const image = new Jimp(imageH, imageW, (err, image) => {
-//   });
-//   console.log(image.bitmap.data.length); 
-//   image.scan(0, 0, imageH, imageW, function(x, y, idx) {
-//     // x, y is the position of this pixel on the image
-//     // idx is the position start position of this rgba tuple in the bitmap Buffer
-//     // this is the image
-   
-//     // var red = this.bitmap.data[idx + 0];
-//     // var green = this.bitmap.data[idx + 1];
-//     // var blue = this.bitmap.data[idx + 2];
-//     // var alpha = this.bitmap.data[idx + 3];
-//     this.bitmap.data[idx + 0] = 100;
-//     // console.log(red, green, blue, alpha);
-   
-//     // rgba values run from 0 - 255
-//     // e.g. this.bitmap.data[idx] = 0; // removes red from this pixel
-//   });
+function deprocessImage(x) {
+  return tf.tidy(() => {
+    const {mean, variance} = tf.moments(x);
+    x = x.sub(mean);
+    x = x.div(tf.sqrt(variance).add(EPSILON));
+    // Clip to [0, 1].
+    x = x.add(0.5);
+    x = tf.clipByValue(x, 0, 1);
+    x = x.mul(255);
+    return tf.clipByValue(x, 0, 255).asType('int32');
+  });
+}
+
+/**
+ * Visualize the maximally-activating input images for a covn2d layer.
+ * 
+ * @param {tf.Model} model The model that the conv2d layer of interest belongs
+ *   to.
+ * @param {string} layerName The name of the layer of interest.
+ * @param {number} numFilters Number of the conv2d layer's filter to calculate
+ *   maximally-activating inputs for. If this exceeds the number of filters
+ *   that the conv2d layer has, it will be cut off.
+ * @param {string} filePathPrefix File path prefix to which the output image
+ *   will be written.
+ * @returns {string[]} Paths to the image files generated in this call.
+ */
+function writeConvLayerFilters(model, layerName, numFilters, iterations, filePathPrefix) {
+  const filePaths = [];
+  tf.tidy(() => {
+    const maxFilters = model.getLayer(layerName).getWeights()[0].shape[3];
+    if (numFilters > maxFilters) {
+      numFilters = maxFilters;
+    }
+    for (let i = 0; i < numFilters; ++i) {
+      console.log(`Processing layer ${layerName}, filter ${i + 1} of ${numFilters}`);
+      const imageTensor = inputGradientAscent(model, layerName, i, iterations);
+      const outputFilePath = `${filePathPrefix}_${layerName}_${i}.png`;
+      filePaths.push(outputFilePath);
+      utils.writeImageTensorToFile(imageTensor, outputFilePath);
+      console.log(`  --> ${outputFilePath}`);
+    }
+  });
+  return filePaths;
+}
+
+function parseArguments() {
+  const parser = new argparse.ArgumentParser({
+    description: 'Visualize convnet'
+  });
+  parser.addArgument('modelJsonUrl', {
+    type: 'string',
+    help: 'URL to model JSON. Can be a file://, http://, or https:// URL'
+  });
+  parser.addArgument('convLayerNames', {
+    type: 'string',
+    help: 'Names of the conv2d layers to visualize, separated by commas ' +
+    'e.g., (block1_conv1,block2_conv1,block3_conv1,block4_conv1)'
+  });
+  parser.addArgument('--outputPathPrefix', {
+    type: 'string',
+    defaultValue: 'dist/filters/filter',
+    help: 'Output file path prefix'
+  });
+  parser.addArgument('--filters', {
+    type: 'int',
+    defaultValue: 64,
+    help: 'Number of filters to visualize for each conv2d layer'
+  });
+  parser.addArgument('--iterations', {
+    type: 'int',
+    defaultValue: 40,
+    help: 'Number of iterations to use for gradient ascent'
+  });
+  parser.addArgument('--gpu', {
+    action: 'storeTrue',
+    help: 'Use tfjs-node-gpu (required CUDA GPU).'
+  });
+  return parser.parseArgs();
 }
 
 async function run() {
-  console.log('Loading model...');
-  // console.log(tf.models.execute);
+  const args = parseArguments();
+  console.log('args:', args);  // DEBUG
 
-  const model = await tf.loadModel(MODEL_PATH);
+  if (args.gpu) {
+    // Use GPU bindings.
+    require('@tensorflow/tfjs-node-gpu');
+  } else {
+    // Use CPU bindings.
+    require('@tensorflow/tfjs-node');
+  }
+
+  console.log('Loading model...');
+  if (args.modelJsonUrl.indexOf('http://') === -1 &&
+      args.modelJsonUrl.indexOf('https://') === -1 &&
+      args.modelJsonUrl.indexOf('file://') === -1) {
+    args.modelJsonUrl = `file://${args.modelJsonUrl}`;
+  }
+  const model = await tf.loadModel(args.modelJsonUrl);
   // const model = await tf.loadModel('./vgg16_tfjs/model.json');
-  model.summary();  //
+  // model.summary();  //
 
   // Warmup the model. This isn't necessary, but makes the first prediction
   // faster. Call `dispose` to release the WebGL memory allocated for the return
@@ -164,8 +222,18 @@ async function run() {
   // MNIST model.
 //   generatePatterns(model, 'max_pooling2d_1', 1);  // Interesting.
 
+  console.log(args.outputPathPrefix);  // DEBUG
+  const outputDirname = path.dirname(args.outputPathPrefix);
+  if (!fs.existsSync(outputDirname)) {
+    shelljs.mkdir('-p', outputDirname);
+  }
+
   // VGG16
-  generatePatterns(model, 'block5_conv3', 0);  // Interesting.
+  // TODO(cais): Handle multiple layers.
+  // inputGradientAscent(model, args.convLayerNames, filterIndex, args.iterations);  // Interesting.
+  writeConvLayerFilters(model, args.convLayerNames, args.filters, args.iterations, args.outputPathPrefix);
+
+  // TODO(cais): 
 };
 
 run();

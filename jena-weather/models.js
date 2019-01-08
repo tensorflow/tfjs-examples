00232ede40009f4f4c814c8ba49546522bd3a72f
Shanqing Cai <cais@google.com>
2019-01-08 14:46:12 -0500

    Add gru-dropout option

--- jena-weather/models.js
+++ jena-weather/models.js
@@ -104,6 +104,10 @@ export function buildModel(modelType, numTimeSteps, numFeatures) {
     model = buildMLPModel(inputShape, regularizer, dropoutRate);
   } else if (modelType === 'gru') {
     model = buildGRUModel(inputShape);
+  } else if (modelType === 'gru-dropout') {
+    const dropoutRate = 0.2;
+    const recurrentDropoutRate = 0.2;
+    model = buildGRUModel(inputShape, dropoutRate, recurrentDropoutRate);
   } else {
     throw new Error(`Unsupported model type: ${modelType}`);
   }

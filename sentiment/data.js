3c59feb467debef637cc87536348ba456d6bdb65
Shanqing Cai <cais@google.com>
2019-01-08 23:56:05 -0500

    Making data.js work

--- sentiment/data.js
+++ sentiment/data.js
@@ -16,21 +16,103 @@
  */
 
 import * as fs from 'fs';
+import * as tf from '@tensorflow/tfjs';
 
-async function main() {
-  const filePath = './python/imdb_train_data.bin';
+const PAD_CHAR = 0;
+const OOV_CHAR = 2;
+const INDEX_FROM = 3;
+
+function padSequence(seq, len) {
+  if (seq.length < len) {
+    const padLen = len - seq.length;
+    for (let n = 0; n < padLen; ++n) {
+      seq.push(PAD_CHAR);
+    }
+  }
+}
+
+function loadFeatures(filePath, numWords, len) {
   const buffer = fs.readFileSync(filePath);
-  let index = 0;
-  while (true) {
-    const value = buffer.readInt32LE(index);
-    console.log(value);
+  const numBytes = buffer.byteLength;
 
-    if (value === 1 && index > 0) {
-      console.log(index);
-      break;
+  let sequences = [];
+  let seq = [];
+  let index = 0;
+
+  while (index < numBytes) {
+    const value = buffer.readInt32LE(index);
+    if (value === 1) {
+      // A new sequence has started.
+      if (index > 0) {
+        padSequence(seq, len);
+        sequences.push(seq.slice(0, len));
+      }
+      seq = [];
+    } else {
+      // Sequence continues.
+      seq.push(value >= numWords ? OOV_CHAR : value);
     }
     index += 4;
   }
+  if (seq.length > 0) {
+    padSequence(seq, len);
+    sequences.push(seq.slice(0, len));
+  }
+  return tf.tensor2d(sequences, [sequences.length, len], 'int32');
+}
+
+function loadTargets(filePath) {
+  const buffer = fs.readFileSync(filePath);
+  const numBytes = buffer.byteLength;
+
+  let ys = [];
+  for (let i = 0; i < numBytes; ++i) {
+    ys.push(buffer.readUInt8(i));
+  }
+  return tf.tensor2d(ys, [ys.length, 1], 'float32');
+}
+
+function loadData(pathPrefix, numWords, len) {
+  const trainFeaturePath = `${pathPrefix}_train_data.bin`;
+  const xTrain = loadFeatures(trainFeaturePath, numWords, len);
+  const testFeaturePath = `${pathPrefix}_test_data.bin`;
+  const xTest = loadFeatures(testFeaturePath,  numWords, len);
+  const trainTargetsPath = `${pathPrefix}_train_targets.bin`;
+  const yTrain = loadTargets(trainTargetsPath);
+  const testTargetsPath = `${pathPrefix}_test_targets.bin`;
+  const yTest = loadTargets(testTargetsPath);
+
+  tf.util.assert(
+      xTrain.shape[0] === yTrain.shape[0],
+      `Mismatch in number of examples between xTrain and yTrain`);
+  tf.util.assert(
+      xTest.shape[0] === yTest.shape[0],
+      `Mismatch in number of examples between xTest and yTest`);
+  return {xTrain, yTrain, xTest, yTest};
+}
+
+async function main() {
+  const numWords = 20000;
+  const len = 100;
+  const {xTrain, yTrain, xTest, yTest} = loadData('./python/imdb', numWords, len);
+  console.log(xTrain.shape);  // DEBUG
+  console.log(xTest.shape);  // DEBUG
+  // const sent = xTrain.gather(0).dataSync();
+  yTrain.min().print();  // DEBUG
+  yTrain.max().print();  // DEBUG
+  yTrain.mean().print();  // DEBUG
+
+  // const metadataPath = 'metadata.json';
+  // const word2Index = JSON.parse(fs.readFileSync(metadataPath, {encoding: 'utf-8'})).word_index;
+  // const index2Word = {};
+  // for (const word in word2Index) {
+  //   // console.log(word);
+  //   index2Word[+word2Index[word]] = word;
+  // }
+  // console.log(index2Word);  // DEBUG
+  // for (const i of sent) {
+  //   console.log(index2Word[i - INDEX_FROM]);  // DEBUG
+  // }
 }
 
 main();

a259d58b1464892eeeb46ac7ce3cd80db5f4b19e
Shanqing Cai <cais@google.com>
2019-01-15 14:15:17 -0500

    Use fitDataset

--- jena-weather/models.js
+++ jena-weather/models.js
@@ -151,74 +151,58 @@ export async function trainModel(
   const shuffle = true;
   const minIndex = 0;
   const maxIndex = 200000;
+
   const trainNextBatchFn = jenaWeatherData.getNextBatchFunction(
       shuffle, lookBack, delay, batchSize, step, minIndex, maxIndex, normalize,
       includeDateTime);
+  const trainDataset = tf.data.generator(trainNextBatchFn).prefetch(8);
+
+  const valMinIndex = 200001;
+  const valMaxIndex = 300000;
+  const valNextBatchFn = jenaWeatherData.getNextBatchFunction(
+      false, lookBack, delay, batchSize, step, valMinIndex, valMaxIndex,
+      normalize, includeDateTime);
+  const valDataset = tf.data.generator(valNextBatchFn);
 
   const batchesPerEpoch = 500;
-  for (let i = 0; i < epochs; ++i) {
-    const t0 = tf.util.now();
-    let totalTrainLoss = 0;
-    let numSeen = 0;
-    for (let j = 0; j < batchesPerEpoch; ++j) {
-      const item = trainNextBatchFn();
-      const trainLoss = await model.trainOnBatch(item.value[0], item.value[1]);
-
-      numSeen += item.value[0].shape[0];
-      totalTrainLoss += item.value[0].shape[0] * trainLoss;
-      if ((j + 1) % displayEvery === 0) {
-        const t = tf.util.now();
-        const millisPerBatch = (t - t0) / (j + 1);
-        console.log(
-            `epoch ${i + 1}/${epochs} batch ${j + 1}/${batchesPerEpoch}: ` +
-            `trainLoss=${trainLoss.toFixed(6)} ` +
-            `(${millisPerBatch.toFixed(1)} ms/batch)`);
-      }
-      tf.dispose(item.value);
-    }
-    const t1 = tf.util.now();
-    const epochTrainLoss = totalTrainLoss / numSeen;
-
-    // Perform validation.
-    const valMinIndex = 200001;
-    const valMaxIndex = 300000;
-    const valNextBatchFn = jenaWeatherData.getNextBatchFunction(
-        false, lookBack, delay, batchSize, step, valMinIndex, valMaxIndex,
-        normalize, includeDateTime);
-    const valT0 = tf.util.now();
-    const valSteps = Math.floor((300000 - 200001 - lookBack) / batchSize);
-    tf.tidy(() => {
-      console.log(`Running validation: valSteps=${valSteps}`);
-      let totalValLoss = tf.scalar(0);
-      numSeen = 0;
-      for (let j = 0; j < valSteps; ++j) {
-        if (j % displayEvery === 0) {
-          console.log(`  Validation: step ${j}/${valSteps}`);
+  let t0;
+  let currentEpoch;
+  await model.fitDataset(trainDataset, {
+    batchesPerEpoch,
+    epochs,
+    callbacks: {
+      onEpochBegin: async (epoch) => {
+        currentEpoch = epoch;
+        t0 = tf.util.now();
+      },
+      onBatchEnd: async (batch, logs) => {
+        if ((batch + 1) % displayEvery === 0) {
+          const t = tf.util.now();
+          const millisPerBatch = (t - t0) / (batch + 1);
+          console.log(
+              `epoch ${currentEpoch + 1}/${epochs} ` +
+              `batch ${batch + 1}/${batchesPerEpoch}: ` +
+              `loss=${logs.loss.toFixed(6)} ` +
+              `(${millisPerBatch.toFixed(1)} ms/batch)`);
         }
-        const item = valNextBatchFn();
-        const evalOut =
-            model.evaluate(item.value[0], item.value[1], {batchSize});
-        const numExamples = item.value[0].shape[0];
-        totalValLoss = tf.tidy(
-            () => totalValLoss.add(evalOut.mulStrict(tf.scalar(numExamples))));
-        numSeen += numExamples;
-        tf.dispose([item.value, evalOut]);
+      },
+      onEpochEnd: async (epoch, logs) => {
+        console.log(`epoch ${epoch + 1}/${epochs}: Performing validation...`);
+        const evalOut = await model.evaluateDataset(valDataset, {
+          batches: 700  // TODO(cais): Do not hard code.
+        });
+        const valLoss = (await evalOut.data())[0];
+        tf.dispose(evalOut);
+        console.log(
+            `epoch ${epoch + 1}/${epochs}: ` +
+            `trainLoss=${logs.loss.toFixed(6)}; ` +
+            `valLoss=${valLoss.toFixed(6)}`);
+        if (epochEndCallback) {
+          epochEndCallback(epoch + 1, logs.loss, valLoss);
+        }
       }
-      const valLoss = totalValLoss.divStrict(tf.scalar(numSeen)).dataSync()[0];
-      const valT1 = tf.util.now();
-      const valMsPerBatch = (valT1 - valT0) / valSteps;
-      console.log(
-          `epoch ${i + 1}/${epochs}: trainLoss=${epochTrainLoss.toFixed(6)}; ` +
-          `valLoss=${valLoss.toFixed(6)} ` +
-          `(train: ${((t1 - t0) / batchesPerEpoch).toFixed(1)} ms/batch; ` +
-          `val: ${valMsPerBatch.toFixed(1)} ms/batch)\n`);
-
-      if (epochEndCallback) {
-        epochEndCallback(i + 1, epochTrainLoss, valLoss)
-      }
-      tf.dispose(valLoss);
-    });
-  }
+    }
+  });
 
   return model;
 }

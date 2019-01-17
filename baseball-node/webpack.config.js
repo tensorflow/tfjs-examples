9dbb319cf1666e7f5906de80ba84f8c1e02448ab
Nick Kreeger <nick.kreeger@gmail.com>
2019-01-17 14:29:34 -0800

    Dev-server config

--- baseball-node/webpack.config.js
+++ baseball-node/webpack.config.js
@@ -19,5 +19,11 @@ const path = require('path');
 
 module.exports = {
   entry: './client.js',
-  output: {path: path.resolve(__dirname, 'dist'), filename: 'bundle.js'}
+  output: {path: path.resolve(__dirname, 'dist'), filename: 'bundle.js'},
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    host: '0.0.0.0',
+    disableHostCheck: true
+  }
 };

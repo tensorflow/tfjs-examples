c249ed8219b71bdcc1e9117717a54bc6618f8e27
Shanqing Cai <cais@google.com>
2019-03-11 15:43:25 -0400

    Back out more tfjs-vis upgrades

--- cart-pole/ui.js
+++ cart-pole/ui.js
@@ -102,7 +102,7 @@ function onIterationEnd(iterationCount, totalIterations) {
 // Objects and function to support the plotting of game steps during training.
 let meanStepValues = [];
 function plotSteps() {
-  tfvis.render.linechart(stepsContainer, {values: meanStepValues}, {
+  tfvis.render.linechart({values: meanStepValues}, stepsContainer, {
     xLabel: 'Training Iteration',
     yLabel: 'Mean Steps Per Game',
     width: 400,

2fcf872ef26e393e382aa6bf309caddfdf77beee
Yannick Assogba <yassogba@google.com>
2019-03-12 17:33:50 -0400

    update mnist core

--- mnist-core/ui.js
+++ mnist-core/ui.js
@@ -35,7 +35,7 @@ export function trainingLog(loss, iteration) {
     yLavel: 'Loss',
   };
   const data = {values: lossArr, series: ['loss']};
-  tfvis.render.linechart(data, container, options);
+  tfvis.render.linechart(container, data, options);
 }
 
 export function showTestResults(batch, predictions, labels) {

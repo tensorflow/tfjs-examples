df97b7208f5097ecb2b215e3351424f552a79ce6
Huan LI <zixia@zixia.net>
2019-03-13 22:43:59 +0800

    Add standard license to TS file

--- translation/translation.ts
+++ translation/translation.ts
@@ -1,3 +1,19 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
 
 /**
  * Train a simple LSTM model for character-level language translation.
@@ -8,11 +24,9 @@
  *   wget http://www.manythings.org/anki/fra-eng.zip
  *
  * Author: Huan LI <zixia@zixia.net>
- * 2018, https://github.com/huan
+ * 2019, https://github.com/huan
  *
  */
-
-
 import fs from 'fs';
 import path from 'path';
 

91ca7c84371c54d7de55cb1f00820fa4a6e27d44
Shanqing Cai <cais@google.com>
2019-03-14 15:53:38 -0400

    Fix test names

--- sentiment/sequence_utils_test.js
+++ sentiment/sequence_utils_test.js
@@ -41,6 +41,13 @@ describe('padSequences', () => {
 
   it('pre pad, pre trucnate', () => {
     const sequences = [[10, 20, 30], [5, 15], [], [1, 2, 3, 4, 5, 6]];
+    const output = padSequences(sequences, 4, 'pre', 'pre');
+    expect(output).toEqual(
+        [[0, 10, 20, 30], [0, 0, 5, 15], [0, 0, 0, 0], [3, 4, 5, 6]]);
+  });
+
+  it('custom padding character', () => {
+    const sequences = [[10, 20, 30], [5, 15], [], [1, 2, 3, 4, 5, 6]];
     const output = padSequences(sequences, 4, 'pre', 'pre', 42);
     expect(output).toEqual(
         [[42, 10, 20, 30], [42, 42, 5, 15], [42, 42, 42, 42], [3, 4, 5, 6]]);

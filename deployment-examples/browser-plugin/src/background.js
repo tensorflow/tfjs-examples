0f556d76a6e58212386147aa0537e9d17798f552
Stanley Bileschi <bileschi@google.com>
2019-06-24 18:19:57 -0400

    wip

--- deployment-examples/browser-plugin/src/background.js
+++ deployment-examples/browser-plugin/src/background.js
@@ -15,38 +15,36 @@
  * =============================================================================
  */
 
-import * as tf from '@tensorflow/tfjs';
+// import * as tf from '@tensorflow/tfjs';
 
-import {IMAGENET_CLASSES} from './imagenet_classes';
+// import {IMAGENET_CLASSES} from './imagenet_classes';
 
-const MOBILENET_MODEL_PATH =
-    // tslint:disable-next-line:max-line-length
-    'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
+// const MOBILENET_MODEL_PATH =
+//     // tslint:disable-next-line:max-line-length
+//     'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
 
-const IMAGE_SIZE = 224;
-const TOPK_PREDICTIONS = 10;
+// const IMAGE_SIZE = 224;
+// const TOPK_PREDICTIONS = 10;
 
-let mobilenet;
+// let mobilenet;
 
-const mobilenetDemo = async () => {
+/**
+ * Loads the model and calls predict on some garbage data to make subsequet
+ * calls faster.
+ */
+/*const loadMobilenetDemo = async () => {
   status('Loading model...');
-
   mobilenet = await tf.loadLayersModel(MOBILENET_MODEL_PATH);
-
-  // Warmup the model. This isn't necessary, but makes the first prediction
-  // faster. Call `dispose` to release the WebGL memory allocated for the return
-  // value of `predict`.
   mobilenet.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
   status('Model warmed up....');
 };
-
+*/
 /**
  * Given an image element, makes a prediction through mobilenet returning the
  * probabilities of the top K classes.
  */
-async function predict(imgElement) {
+/*async function predict(imgElement) {
   status('Predicting...');
-
   // The first start time includes the time it takes to extract the image
   // from the HTML and preprocess it, in additon to the predict() call.
   const startTime1 = performance.now();
@@ -56,30 +54,48 @@ async function predict(imgElement) {
   const logits = tf.tidy(() => {
     // tf.browser.fromPixels() returns a Tensor from an image element.
     const img = tf.browser.fromPixels(imgElement).toFloat();
-
     const offset = tf.scalar(127.5);
     // Normalize the image from [0, 255] to [-1, 1].
     const normalized = img.sub(offset).div(offset);
-
     // Reshape to a single-element batch so we can pass it to predict.
     const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
-
     startTime2 = performance.now();
     // Make a prediction through mobilenet.
     return mobilenet.predict(batched);
   });
-
   // Convert logits to probabilities and class names.
   const classes = await getTopKClasses(logits, TOPK_PREDICTIONS);
   const totalTime1 = performance.now() - startTime1;
   const totalTime2 = performance.now() - startTime2;
   status(`Done in ${Math.floor(totalTime1)} ms ` +
       `(not including preprocessing: ${Math.floor(totalTime2)} ms)`);
-
   // Show the classes in the DOM.
   showResults(imgElement, classes);
 }
+*/
 
+function handleImageURL(url) {
+  console.log('IM HANDLING IM HANDLING');
+  // TODO: Create a div element in the main page's DOM
+  // TODO: Load the image into that div
+  // TODO: Execute `predict` on the image element.
+};
+
+/**
+ * Illustrates the results (currently logs to console).
+ * @param {*} imgElement
+ * @param {*} classes
+ */
+/*function showResults(imgElement, classes) {
+  console.log("I'm showResults!");
+  for (let i = 0; i < TOPK_PREDICTIONS; i++) {
+    console.log(IMAGENET_CLASSES[i]);
+  }
+}
+*/
+const status = (x) => console.log(x);
+
+//loadMobilenetDemo();
 
 /**
  * @fileoverview Description of this file.
@@ -91,12 +107,19 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'stackoverflow.com'},
+        pageUrl: {hostEquals: 'google.com'},
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 });
+
+chrome.runtime.onMessage.addListener(
+  function(message, callback) {
+    if (message == "predict") {
+    }
+  }
+);
 
 chrome.contextMenus.create({
   title: "Use URL of image somehow",
@@ -107,20 +130,3 @@ chrome.contextMenus.create({
   }
 });
 
-function handleImageURL(url) {
-  console.log('IM HANDLING IM HANDLING');
-  // Create a div element in the main page's DOM
-  // Load the image into that div
-  // Execute `predict` on the image element.
-};
-
-function showResults(imgElement, classes) {
-  console.log("I'm showResults!");
-  for (let i = 0; i < TOPK_PREDICTIONS; i++) {
-    console.log(IMAGENET_CLASSES[i]);
-  }
-}
-
-const status = (x) => console.log(x);
-
-mobilenetDemo();

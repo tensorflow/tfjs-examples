49a9e64ba37df16f54300b632e0a4809d791b272
Clément Beauseigneur <cbeausei@google.com>
2020-10-08 18:30:16 +0200

    [Interactive Visualizer] Add logic to fetch model, metadata, test data, and display test images

--- interactive-visualizers/src/app/app.component.spec.ts
+++ interactive-visualizers/src/app/app.component.spec.ts
@@ -15,34 +15,113 @@
  * =============================================================================
  */
 
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import * as tf from '@tensorflow/tfjs';
+import fetchMock from 'fetch-mock';
+
+import {AppComponent} from './app.component';
+
+const weightsManifest: tf.io.WeightsManifestEntry[] =
+    [{'name': 'Const', 'dtype': 'int32', 'shape': [1]}];
+
+const SIMPLE_MODEL = {
+  node: [
+    {
+      name: 'Input',
+      op: 'Placeholder',
+      attr: {
+        dtype: {
+          type: 3,
+        },
+        shape: {shape: {dim: [{size: -1}, {size: 1}]}}
+      }
+    },
+    {
+      name: 'Const',
+      op: 'Const',
+      attr: {
+        dtype: {type: 3},
+        value: {
+          tensor: {
+            dtype: 3,
+            tensorShape: {dim: [{size: 1}]},
+          }
+        },
+        index: {i: 0},
+        length: {i: 4}
+      }
+    },
+    {name: 'Add1', op: 'Add', input: ['Input', 'Const'], attr: {}},
+    {name: 'Add', op: 'Add', input: ['Add1', 'Const'], attr: {}}
+  ],
+  versions: {producer: 1.0, minConsumer: 3}
+};
 
 describe('AppComponent', () => {
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    await TestBed
+        .configureTestingModule({
+          declarations: [AppComponent],
+        })
+        .compileComponents();
   });
 
-  it('should create the app', () => {
+  afterEach(() => {
+    fetchMock.reset();
+  });
+
+  it('app should be successfully created', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'interactive-visualizers'`, () => {
+  it('app should trigger an error without metadata URL query parameter', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('interactive-visualizers');
+    expect(app.ngOnInit).toThrowError();
   });
 
-  it('should render title', () => {
+  it('fetchModel should correctly parse metadata', async () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('interactive-visualizers app is running!');
+    const app = fixture.componentInstance;
+
+    // Prepare test data.
+    const fakeModelMetadataUrl = 'https://modelMetadataUrl/metadata.json';
+    const fakeModelUrl = 'https://modelMetadataUrl/model.json';
+    const fakeTestImagesIndexPath = 'https://testImagesPath/index.json';
+    const fakeModelMetadata = {
+      'tfjs_classifier_model_metadata': {},
+      'test_images_index_path': fakeTestImagesIndexPath,
+    };
+    const customLoader: tf.io.IOHandler = {
+      load: async () => {
+        return {
+          modelTopology: SIMPLE_MODEL,
+          weightSpecs: weightsManifest,
+          weightData: new Int32Array([5]).buffer,
+        };
+      }
+    };
+    const simpleModel = await tf.loadGraphModel(customLoader);
+
+    // Mock calls.
+    fetchMock.get(fakeModelMetadataUrl, fakeModelMetadata);
+    spyOn(app, 'fetchModel').and.returnValue(Promise.resolve(simpleModel));
+    fetchMock.get(fakeTestImagesIndexPath, ['image1.jpg', 'image2.jpg']);
+
+    await app.initApp(fakeModelMetadataUrl);
+
+    expect(app.modelType).toEqual('classifier');
+    expect(app.testImages).toEqual([
+      {
+        imageUrl: 'https://testImagesPath/image1.jpg',
+        thumbnailUrl: 'https://testImagesPath/image1_thumb.jpg',
+      },
+      {
+        imageUrl: 'https://testImagesPath/image2.jpg',
+        thumbnailUrl: 'https://testImagesPath/image2_thumb.jpg',
+      },
+    ]);
   });
 });
--- interactive-visualizers/src/app/app.component.ts
+++ interactive-visualizers/src/app/app.component.ts
@@ -15,13 +15,107 @@
  * =============================================================================
  */
 
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+
+// Messages.
+const DOWNLOAD_MESSAGE =
+    'This interactive model visualizer will run in the browser: the required TensorFlow.js model files are currently being loaded. Thanks for your patience!';
+const NO_MODEL_METADATA_ERROR_MESSAGE =
+    'No model metadata URL provided. The visualizer could not start';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'interactive-visualizers';
+
+  // Model related variables.
+  modelMetadataUrl: string|null = null;
+  modelMetadata: {}|null = null;
+  modelType: string|null = null;
+  model: tf.GraphModel|null = null;
+  labelmap: string[] = [];
+  defaultScoreThreshold: number = 0.0;
+
+  // Test data related variables.
+  testImagesIndexUrl: string|null = null;
+  testImages: Array<{imageUrl: string, thumbnailUrl: string}> = [];
+  uploadedImages: string[] = [];
+
+  ngOnInit() {
+    // Sanity checks on URL query parameters.
+    const urlParams = new URLSearchParams(window.location.search);
+    if (!urlParams.has('modelMetadataUrl')) {
+      throw new Error(NO_MODEL_METADATA_ERROR_MESSAGE);
+    }
+    const modelMetadataUrl = urlParams.get('modelMetadataUrl') || '';
+
+    this.initApp(modelMetadataUrl);
+
+    this.refresh();
+  }
+
+  /**
+   * Non-async refresh method to force re-evaluation of consitional statements in templates (such as `ngIf`-s). This is a workaround as variable changes performed in async code do not trigger re-evaluation, which seems to be caused by a Zone.js issue with ES2017 (see https://github.com/angular/angular/issues/31730).
+   */
+  refresh() {
+    setTimeout(() => this.refresh(), 100);
+  }
+
+  async fetchModel(modelUrl) {
+    const model = await tf.loadGraphModel(modelUrl);
+    return model;
+  }
+
+  /**
+   * Initializes the app for the provided model metadata URL.
+   */
+  async initApp(modelMetadataUrl: string) {
+    // Load model & metadata.
+    this.modelMetadataUrl = modelMetadataUrl;
+    const metadataResponse = await fetch(this.modelMetadataUrl);
+    this.modelMetadata = await metadataResponse.json();
+    const modelUrl = this.getAssetsUrlPrefix() + 'model.json';
+    this.model = await this.fetchModel(modelUrl);
+    if (this.modelMetadata['tfjs_classifier_model_metadata']) {
+      this.modelType = 'classifier';
+    }
+
+    // Fetch test data if any.
+    this.testImagesIndexUrl = this.modelMetadata['test_images_index_path'];
+    try {
+      const testImagesResponse = await fetch(this.testImagesIndexUrl);
+      const testImageNames = await testImagesResponse.json();
+      this.testImages = [];
+      for (const testImageName of testImageNames) {
+        const testImageExt =
+            testImageName.split('.')[testImageName.split('.').length - 1];
+        const testImageNamePrefix = testImageName.split('.' + testImageExt)[0];
+        this.testImages.push({
+          imageUrl: this.getTestImagesUrlPrefix() + testImageName,
+          thumbnailUrl: this.getTestImagesUrlPrefix() + testImageNamePrefix +
+              '_thumb.' + testImageExt,
+        });
+      }
+
+    } catch (error) {
+      console.error(`Couldn't fetch test images: ${error}`);
+    }
+  }
+
+  getAssetsUrlPrefix(): string {
+    const metadataFileName = this.modelMetadataUrl.split(
+        '/')[this.modelMetadataUrl.split('/').length - 1];
+    return this.modelMetadataUrl.split(metadataFileName)[0];
+  }
+
+  getTestImagesUrlPrefix(): string {
+    const imageIndexFileName = this.testImagesIndexUrl.split(
+        '/')[this.testImagesIndexUrl.split('/').length - 1];
+    return this.testImagesIndexUrl.split(imageIndexFileName)[0];
+  }
 }

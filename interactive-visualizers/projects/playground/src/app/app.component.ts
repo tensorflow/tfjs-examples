0fdb13d7872afec164e485771dcfe8a847c7d011
Clément Beauseigneur <cbeausei@google.com>
2020-11-25 18:07:23 +0100

    Fix alignement

--- interactive-visualizers/projects/playground/src/app/app.component.ts
+++ interactive-visualizers/projects/playground/src/app/app.component.ts
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
- import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
--- interactive-visualizers/projects/playground/src/polyfills.ts
+++ interactive-visualizers/projects/playground/src/polyfills.ts
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
- /**
+/**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
  *

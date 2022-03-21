115df3a17287cb8fe234c90252acca24eefee027
Ping Yu <4018+pyu10055@users.noreply.github.com>
2022-03-21 12:56:10 -0700

    update all package.json for security patches

--- baseball-node/webpack.config.js
+++ baseball-node/webpack.config.js
@@ -20,10 +20,5 @@ const path = require('path');
 module.exports = {
   entry: './client.js',
   output: {path: path.resolve(__dirname, 'dist'), filename: 'bundle.js'},
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    host: '0.0.0.0',
-    disableHostCheck: true
-  }
+  devServer: {historyApiFallback: true, host: '0.0.0.0'}
 };
